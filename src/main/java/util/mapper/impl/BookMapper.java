f2ce2de9b91d16344122af6041058c71571fb78a
Сергей <sergeiik123@yandex.ru>
2024-04-19 14:15:14 +0300

    Work1

---
+++ src/main/java/util/mapper/impl/BookMapper.java
@@ -0,0 +1,18 @@
+package util.mapper.impl;
+
+import model.Library;
+import util.mapper.Mapper;
+
+public class BookMapper implements Mapper {
+
+    @Override
+    public String toInput(Library var1) {
+        return null;
+    }
+
+    @Override
+    public Library toOutput(String var1) {
+        return null;
+    }
+}
+
